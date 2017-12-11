abb633b8165a6a11dbc2087b9eea25b9562e0569
Mac <jordan.dale.cummings@gmail.com>
2017-12-11 13:49:32 -0700

    Created and styled out entire app (I had forgotten to push for a few days)

--- app/app.module.js
+++ app/app.module.js
@@ -8,20 +8,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
-var homepage_component_1 = require("./homepage.component");
+var forms_1 = require("@angular/forms");
+var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
+var app_component_1 = require("./app.component");
+var homepage_component_1 = require("./homepage/homepage.component");
+var documents_component_1 = require("./documents/documents.component");
+var proposal_list_component_1 = require("./proposal/proposal-list.component");
+var proposal_new_component_1 = require("./proposal/proposal-new.component");
+var proposal_show_component_1 = require("./proposal/proposal-show.component");
+var app_routing_module_1 = require("./app-routing.module");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [
-                platform_browser_1.BrowserModule
+                platform_browser_1.BrowserModule,
+                app_routing_module_1.AppRoutingModule,
+                forms_1.FormsModule,
+                ng_bootstrap_1.NgbModule.forRoot()
             ],
             declarations: [
-                homepage_component_1.HomepageComponent
+                app_component_1.AppComponent,
+                homepage_component_1.HomepageComponent,
+                documents_component_1.DocumentsComponent,
+                proposal_list_component_1.ProposalListComponent,
+                proposal_new_component_1.ProposalNewComponent,
+                proposal_show_component_1.ProposalShowComponent
             ],
             bootstrap: [
-                homepage_component_1.HomepageComponent
+                app_component_1.AppComponent
             ]
         })
     ], AppModule);
--- app/homepage.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'main-app',
-  template: '<h1>Freelance Bootcamp Dashboard</h1>'
-})
-export class HomepageComponent {}
